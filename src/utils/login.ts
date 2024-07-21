fd3ff5c6db4a60270b48338050ff348a7c3dd038
kssampson <kylesampsonmusic@gmail.com>
2024-07-20 21:45:37 -0700

    clean up console logs

--- src/utils/login.ts
+++ src/utils/login.ts
@@ -3,11 +3,9 @@ import axios from "axios";
 const login = async (email: string, password: string) => {
   try {
     const response = await axios.post('http://localhost:3001/auth/log-in', {email, password})
-    //Todo: determine what to do with the repsonse
     return response
   } catch(error){
-    console.log('Failed on front end: ', error)
-    throw error;
+    throw new Error;
   }
 }
 
--- src/utils/verifiedLogin.ts
+++ src/utils/verifiedLogin.ts
@@ -1,14 +1,11 @@
 import axios from "axios";
 
 const verifiedLogin = async (email: string, password: string, token: string | null) => {
-  // console.log('verifiedlogin handler: ', email, password, token)
   try {
     const response = await axios.post('http://localhost:3001/auth/verified-log-in', {email, password, token})
-    // console.log('response: ', response)
     return response;
   } catch(error){
-    console.log('Failed on front end: ', error)
-    throw error;
+    throw new Error;
   }
 }
 
